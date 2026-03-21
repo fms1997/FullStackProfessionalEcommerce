f95e00e8c02f40646d6a579e30ab8f56907e50d6
Franco Sassi <franco.sassi@brio.com.ar>
2026-03-21 00:06:50 -0300

    20/21-03 pruebas etapa9

--- FulSpectrum/FulSpectrum.Application/obj/Debug/net10.0/FulSpectrum.Application.AssemblyInfo.cs
+++ FulSpectrum/FulSpectrum.Application/obj/Debug/net10.0/FulSpectrum.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FulSpectrum.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e36eeba256b9d123422eed952c719f18a78fd6d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bc8a9386b9367e44bab6e866839d195352006b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FulSpectrum.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FulSpectrum.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- FulSpectrum/FulSpectrum.Infrastructure/obj/Debug/net10.0/FulSpectrum.Infrastructure.AssemblyInfo.cs
+++ FulSpectrum/FulSpectrum.Infrastructure/obj/Debug/net10.0/FulSpectrum.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FulSpectrum.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e36eeba256b9d123422eed952c719f18a78fd6d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bc8a9386b9367e44bab6e866839d195352006b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FulSpectrum.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FulSpectrum.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
